8a26a915ab0d8f322e409d61b0a2a6a744ba93b1
evan <909798432@qq.com>
2015-10-23 01:35:00 +0800

    add net photo, using base.html

--- myalbum/views.py
+++ myalbum/views.py
@@ -4,4 +4,6 @@ from django.shortcuts import render
 # Create your views here.
 
 def myalbum(request):
-    return render(request, u'myalbum.html')
+    photo_list = [u"http://pic.meizitu.com/wp-content/uploads/2015a/10/27/01.jpg",
+                  u"http://pic.meizitu.com/wp-content/uploads/2015a/10/24/01.jpg"]
+    return render(request, u'myalbum.html', {'photo_list': photo_list})
