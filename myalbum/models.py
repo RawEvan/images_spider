51894320c91ae22f50de31f25dedf037a52d86f8
evan <909798432@qq.com>
2015-11-09 00:29:25 +0800

    little change

--- myalbum/models.py
+++ myalbum/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class ImgStorage(models.Model):
+class imgstorage(models.Model):
     original_url = models.CharField(max_length = 200, unique = True)
     storage_url = models.CharField(max_length = 200)
     
